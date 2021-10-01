75e7fb3f3aed5376bef656d32cdef21f40adfdb5
Gopi-Subedi <gopi.subedi09@gmail.com>
2021-10-01 09:16:30 +0545

    output displayed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [bookList, setBookList] = useState("");
+  const [bookList, setBookList] = useState(null);
 
   // useEffect(() => {
   //   fetch("/msg")
@@ -15,9 +15,33 @@ function App() {
         console.log(res);
         return res.json();
       })
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        const result = Object.values(data);
+        // console.log(result);
+        // result.map((re) => console.log(re));
+        setBookList(result);
+
+        // setBookList(data);
+      });
   }, []);
-  return <div className="App"></div>;
+
+  console.log(bookList);
+  return (
+    <div className="App">
+      {bookList ? (
+        <div>
+          <h1>LIST OF BOOK </h1>
+          {/* {bookList[0]["Book-Title"]} */}
+          {bookList.map((book) => (
+            <h1>{book["Book-Title"]}</h1>
+          ))}
+        </div>
+      ) : (
+        <h1>no books</h1>
+      )}
+    </div>
+  );
 }
 
 export default App;
